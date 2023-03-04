33d8fcf845a503c0632e655c6264c4ba09ea9b99
RadiaJannat <101438221+RadiaJannat@users.noreply.github.com>
2023-03-03 21:37:24 -0700

    final package

---
+++ JFreeChart_Lab3/src/org/jfree/data/TestSuite.java
@@ -0,0 +1,15 @@
+package org.jfree.data;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	RangeTest.class,
+	DataUtilitiesTest.class
+	})
+
+public class TestSuite {
+	
+	
+}
